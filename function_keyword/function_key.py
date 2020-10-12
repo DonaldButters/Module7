14e33940de944e2fc446e9b15f49d3326b95209b
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-12 13:57:42 -0500

    function key

--- function_keyword/function_key.py
+++ function_keyword/function_key.py
@@ -6,7 +6,7 @@ Author: Donald Butters
 
 Last date modified: 10/12/2020
 
-The purpose of the program is to make three function calls each with different number of scors and key word arguments
+The purpose of the program is to make three function calls each with different number of scores and key word arguments
 and to output the numbers entered as a list
 
 
@@ -16,19 +16,17 @@ and to output the numbers entered as a list
 
  """
 
+
 def average_steps(*args, **kwargs):
-    # Use *args for average calculation
     total = 0
     items = 0
     print('Pedometer Results: ')
     for key, value in kwargs.items():
         print(str(key) + ' - ' + str(value))
 
-        #print("%s == %s" % (key, value))
     for i in args:
         total = total + int(i)
         items = items + 1
-        #print(str(total))
 
     average = total / items
 
@@ -41,4 +39,3 @@ if __name__ == 'main':
     print(average_steps(2501, 6789, 8952, 40145, 10995, 23914, weight='169', name='Donald', age='77'))
     print(average_steps(17125, 15622, 23584, weight='230', name='Daniel'))
     print(average_steps(500, 500, 500, 500, weight='419', name='Tubby', exwives='55'))
-
