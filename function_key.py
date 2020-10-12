e1e8048416595d69b4e6aedd2b805cfd10f43b4a
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-12 13:59:00 -0500

    Add files via upload
    commit with function key

---
+++ function_key.py
@@ -0,0 +1,41 @@
+"""
+
+Program: function_key.py
+
+Author: Donald Butters
+
+Last date modified: 10/12/2020
+
+The purpose of the program is to make three function calls each with different number of scores and key word arguments
+and to output the numbers entered as a list
+
+
+ :param parameter_1: use arbitrary arguments list and keyword arguments
+ :param parameter_2: make 3 function calls to each
+ :returns: a string in a specific format
+
+ """
+
+
+def average_steps(*args, **kwargs):
+    total = 0
+    items = 0
+    print('Pedometer Results: ')
+    for key, value in kwargs.items():
+        print(str(key) + ' - ' + str(value))
+
+    for i in args:
+        total = total + int(i)
+        items = items + 1
+
+    average = total / items
+
+    print('Your average steps over the past ' + str(items) + ' days is: ' + str(average))
+
+    # return
+
+
+if __name__ == 'main':
+    print(average_steps(2501, 6789, 8952, 40145, 10995, 23914, weight='169', name='Donald', age='77'))
+    print(average_steps(17125, 15622, 23584, weight='230', name='Daniel'))
+    print(average_steps(500, 500, 500, 500, weight='419', name='Tubby', exwives='55'))
