27fe36d06f3c4f194d11dd89f69e8582446e6445
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-12 12:27:57 -0500

    Add files via upload
    basic list and test basic list passing first test

---
+++ basic_list.py
@@ -0,0 +1,13 @@
+def make_list():
+    for i in range(0, 3):
+        a = 5
+        input_list.append(a)
+    print(input_list)
+    return input_list
+
+def get_input():
+    pass
+
+
+input_list = []
+make_list()
